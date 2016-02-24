1abccb83d69c6c16c26d890dbec1a8a2c0ce0614
Gadurp1 <anthony@anthonys-MacBook-Pro.local>
2016-02-24 16:41:36 -0600

    data dump

--- app/Http/Controllers/WebTrackController.php
+++ app/Http/Controllers/WebTrackController.php
@@ -11,17 +11,36 @@ use Goutte\Client;
 class WebTrackController extends Controller
 {
 
-    public function index()
-    {
-        //
-        //$photos=WebTrack::paginate(50);
-        //$photos=WebTrack::paginate(50);
-        $client = new Client();
-        $baseURL = 'http://www.cutecatgifs.com/';
-        $urlEndpoint = '/page/74/';
-        $crawler = $client->request('GET', $baseURL . $urlEndpoint);
-        $photos = $crawler->filter('img.alignnone')->extract(array('src','alt'));
-        return var_dump($photos);
-      }
-
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    for($i=68;$i<=78;$i++){
+      //$photos=Image::paginate(50);
+      $client = new Client();
+      $baseURL = 'http://www.cutecatgifs.com/page';
+      $urlEndpoint = '/'.$i++;
+      $crawler = $client->request('GET', $baseURL . $urlEndpoint);
+      $photos = $crawler->filter('img.alignnone')->extract(array('src','alt'));
+      foreach($photos as $photo){
+        $url = $photo[0];
+        $title =$photo[1];
+        $extension = pathinfo($url, PATHINFO_EXTENSION);
+        $filename = str_random(4).'-'.str_slug($title).'.'. $extension;
+        // get file content from url and save in directory
+        $file = file_get_contents($url);
+        $save_file = file_put_contents('uploads/'.$filename, $file);
+        // Create record in database
+        $image=new \App\Image();
+        $image->name=$photo[1];
+        $image->url=$filename;
+        $image->user_id=1;
+        $image->description=$photo[1];
+        $image->save();
+     }
+   }
+  }
 }
